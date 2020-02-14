11d5949fe9aade9fd7f63f3fc68d1958850c1dff
jamespnixon <58857800+jamespnixon@users.noreply.github.com>
2020-02-13 17:54:39 -0800

    Setup new classes and start of get selected object code

--- WAAPI_Selected_Switch_Assign/Program.cs
+++ WAAPI_Selected_Switch_Assign/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace WAAPI_Selected_Switch_Assign
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _Main().Wait();
+        }
+
+        static async Task _Main()
+        {
+
+            var client = CreateConnection().Result;
+
+
+            await GetSelectedSwitchContainer(client);
+            await client.Close();
+
+          
+        }
+
+        private static async Task GetSelectedSwitchContainer(AK.Wwise.Waapi.JsonClient client)
+        {
+            SelectedSwitchContainer selectedSwitchContainer = new SelectedSwitchContainer();
+
+            var options = new JObject
+                (
+                    new JProperty("return", new string[] { "name", "id", "type"})
+                );    
+
+            try
+            {
+                var results = await client.Call(
+                    ak.wwise.ui.getSelectedObjects,
+                    null,
+                    options);
+
+                Console.WriteLine(results);
+                Console.WriteLine();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private static async Task<AK.Wwise.Waapi.JsonClient> CreateConnection()
+        {
+
+            AK.Wwise.Waapi.JsonClient client = new AK.Wwise.Waapi.JsonClient();
+
+            await client.Connect();
+
+            client.Disconnected += () =>
+            {
+                System.Console.WriteLine("We lost connection!");
+            };
+
+            return client;
         }
     }
 }
---
+++ WAAPI_Selected_Switch_Assign/SelectedSwitchContainer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WAAPI_Selected_Switch_Assign
+{
+    class SelectedSwitchContainer
+    {
+
+        public string name { get; set; }
+
+        public string id { get; set; }
+
+        public string switchGroupOrStateGroup { get; set; }
+
+        public List<SwitchContainerChild> children = new List<SwitchContainerChild>();
+    }
+
+    public class SwitchContainerChild
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+
+        public string stateOrSwitch { get; set; }
+
+
+    }
+
+    public class WwiseSwitch
+    {
+
+    public string name { get; set; }
+    public string id { get; set; }
+    
+    }
+
+}
