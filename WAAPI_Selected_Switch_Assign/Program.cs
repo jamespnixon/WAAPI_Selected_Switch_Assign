8ee893de2cf4bf32292791e1d5ac89f11272f943
jamespnixon <58857800+jamespnixon@users.noreply.github.com>
2020-02-13 16:03:55 -0800

    commit 1
    commit 1

---
+++ WAAPI_Selected_Switch_Assign/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WAAPI_Selected_Switch_Assign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
